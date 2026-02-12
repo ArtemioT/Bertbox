acf02ad5c60cfe463d210940460606ece98a40f0
Artemio Tamayo <arttam@tokyo.cs.colostate.edu>
2026-02-12 14:18:04 -0700

    added comments

--- src/convert.py
+++ src/convert.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import os
 
+#put the file you want to convert into data folder and specify your path in file
 file = "data/real.xls"
 
 df = pd.read_excel(file, engine="xlrd")
-df.to_csv("data/converted.csv")
+
+df.to_csv("data/converted.csv") #put the name of the .csv into this string
